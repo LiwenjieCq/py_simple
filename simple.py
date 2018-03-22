f122354f00a66963c079c7ee9d03e642691ddb62
LiwenjieCq <liwenjiecq@gmail.com>
2018-03-22 10:41:20 +0800

    simple

---
+++ simple.py
@@ -0,0 +1,10 @@
+print("hello","world")
+print("100 + 200=" , 100 + 200)
+# name = input("Please input your name:")
+# print(name)
+print('I\'m learning \npython')
+# \\表示的字符就是\
+print('\\\n\\')
+
+print(r"\\\n\\")
+
